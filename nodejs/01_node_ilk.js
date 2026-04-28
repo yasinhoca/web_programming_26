ca0dd03eb2f8e44cca773d0a0bd3c7ae0c17f873
Yasin Hoca <myasinozsaglam@gmail.com>
2026-04-28 14:48:50 +0300

    Add files via upload

---
+++ nodejs/01_node_ilk.js
@@ -0,0 +1,7 @@
+var http = require('http');
+
+
+http.createServer(function (req,res){
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'} );
+    res.end('NodeJS derslerine hoşgeldiniz!');
+}).listen(8080);
